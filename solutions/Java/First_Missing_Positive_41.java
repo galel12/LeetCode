a1dfdcd29504dae941d7ef03f2dd5e48c6d7d016
galel12 <132947106+galel12@users.noreply.github.com>
2025-02-23 23:46:56 +0200

    First Missing Positive update

--- solutions/Java/First_Missing_Positive_41.java
+++ solutions/Java/First_Missing_Positive_41.java
@@ -5,22 +5,26 @@ public class First_Missing_Positive_41 {
             
             int n = nums.length, curr, next;
             
+            /* Place Numbers in Their Correct Positions: 
+               Loop through the array and swap numbers to their correct position if they are in the range [1, n].
+             */
             for(int i = 0; i < n; i++)
             {
                 curr = nums[i];
                 while((curr >= 1 && curr <= n) && curr != (next = nums[curr - 1]))
                 {
-                    nums[curr - 1] = curr;
-                    curr = next;
+                    nums[curr - 1] = curr;      // Place curr in its correct index (curr - 1)
+                    curr = next;                // Move to the swapped number
                 }
             }
-            
+
+            // After reordering, iterate the array again to check which number is out of place.
             for(int i = 0; i < n; i++)
             {
                 if(nums[i] != i + 1)
-                    return i + 1;
+                    return i + 1; // First missing positive found
             }
-            return n + 1;
+            return n + 1; // If all numbers are in correct positions, return n + 1
         }
     }
 }
