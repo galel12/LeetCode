082142481040e82ff6f454a870ba389c1813121b
galel12 <132947106+galel12@users.noreply.github.com>
2025-02-25 17:12:33 +0200

    Update leetcode #424

--- solutions/python/LongestRepeatingCharacterReplacement.py
+++ solutions/python/LongestRepeatingCharacterReplacement.py
@@ -1,21 +1,23 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         count = {}
-        maxCount = 0
+        maxFreq = 0
         maxLength = 0
         start = 0
         
         for end in range(len(s)):
-            count[s[end]] = 1 + count.get(s[end], 0)
-            maxCount = max(maxCount, count[s[end]])
+            count[s[end]] = 1 + count.get(s[end], 0)    # count the frequency of each character 
+            maxFreq = max(maxFreq, count[s[end]])       # update the maximum frequency of a character
 
-            # If the number of replacements needed to make the substring
-            # from start to end inclusive have more than k replacements,
-            # we need to shrink the window           
-            if (end - start + 1) - maxCount > k:
-                count[s[start]] -= 1 # Remove the character at the start of the window
-                start += 1
+            """
+            If the number of replacements needed to make the substring
+            from start to end inclusive have more than k replacements,
+            we need to shrink the window    
+            """      
+            if (end - start + 1) - maxFreq > k:
+                count[s[start]] -= 1    # decrement the character frequency at the start of the window
+                start += 1 
             
-            maxLength = max(maxLength, end - start + 1)
+            maxLength = max(maxLength, end - start + 1) 
             
         return maxLength
